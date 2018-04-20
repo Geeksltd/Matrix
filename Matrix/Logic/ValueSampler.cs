359880ba2e6ca84150056d7df4f6b55405a24fd6
Mohsen <mohsens2@outlook.com>
2018-04-20 20:39:51 +0430

    cleanup

--- Matrix/Logic/ValueSampler.cs
+++ Matrix/Logic/ValueSampler.cs
@@ -14,17 +14,11 @@ namespace Matrix.Logic
             object result = null;
 
             if (t == typeof(int) || t == typeof(short) || t == typeof(int) || t == typeof(long) || t == typeof(sbyte))
-            {
                 result = rnd.Next(-99, 99);
-            }
             else if (t == typeof(uint) || t == typeof(ushort) || t == typeof(uint) || t == typeof(ulong) || t == typeof(byte))
-            {
                 result = rnd.Next(0, 99);
-            }
             else if (t == typeof(double))
-            {
                 result = Math.Round(10 * rnd.NextDouble(), 2);
-            }
             else if (t == typeof(DateTime))
             {
                 var start = new DateTime(1995, 1, 1);
